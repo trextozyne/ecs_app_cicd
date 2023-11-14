bc9c74bacd6f4a964b08850d57fa85f9fcc04fa4
Tosin Akinnifesi <dalesk1987@gmail.com>
2023-11-14 16:53:52 -0600

    added provider.tf

---
+++ env/dev/provider.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.24.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  profile = "djemmy+2"
+  region  = "us-east-2"
+  # access_key = ""
+  # secret_key = ""
+}
+
